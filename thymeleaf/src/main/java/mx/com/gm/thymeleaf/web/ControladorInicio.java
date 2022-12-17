c6bcf1743a10f5388e250015ff786191b7870bd5
GaboSO21 <gso2090@gmail.com>
2022-12-16 18:59:36 -0800

    Test

--- thymeleaf/src/main/java/mx/com/gm/thymeleaf/web/ControladorInicio.java
+++ thymeleaf/src/main/java/mx/com/gm/thymeleaf/web/ControladorInicio.java
@@ -1,49 +1,61 @@
 package mx.com.gm.thymeleaf.web;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
+// import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.var;
-import lombok.extern.slf4j.Slf4j;
-import mx.com.gm.thymeleaf.domain.Persona;
+//import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.dao.PersonaDao;
+//import mx.com.gm.thymeleaf.domain.Persona;
 
 @Controller
-@Slf4j
+//@Slf4j
 public class ControladorInicio {
+
+    
+    private PersonaDao personaDao;
 
     // @Value("${index.saludo}")
     // private String saludo;    
 
     @GetMapping("/")
     public String inicio(Model model) {    
-        var mensaje = "Hola hola con thymeleaf";
 
-        var persona = new Persona();
-        persona.setNombre("Gabriel");
-        persona.setApellido("Sanchez");
-        persona.setEmail("gso2090@gmail.com");
-        persona.setTelefono("123123123");
-        var persona2 = new Persona();
-        persona2.setNombre("Pedro");
-        persona2.setApellido("Pipi");
-        persona2.setEmail("trolloosoorigen@gmail.com");
-        persona2.setTelefono("8363617");
+        var personas = personaDao.findAll();
+
+
+        // var mensaje = "Hola hola con thymeleaf";
+
+        // var persona = new Persona();
+        // persona.setNombre("Gabriel");
+        // persona.setApellido("Sanchez");
+        // persona.setEmail("gso2090@gmail.com");
+        // persona.setTelefono("123123123");
+        // var persona2 = new Persona();
+        // persona2.setNombre("Pedro");
+        // persona2.setApellido("Pipi");
+        // persona2.setEmail("trolloosoorigen@gmail.com");
+        // persona2.setTelefono("8363617");
         
-        List<Persona> personas = new ArrayList<>();
-        personas.add(persona);
-        personas.add(persona2);
+        // List<Persona> personas = new ArrayList<>();
+        // personas.add(persona);
+        // personas.add(persona2);
 
-        log.info("ejecutando controaldor Spring MVC");
-        // model.addAttribute("persona2", persona2);
-        // model.addAttribute("persona", persona);
+        // log.info("ejecutando controaldor Spring MVC");
+        // // model.addAttribute("persona2", persona2);
+        // // model.addAttribute("persona", persona);
         model.addAttribute("personas", personas);
-        model.addAttribute("mensaje", mensaje);
-        // model.addAttribute("saludo", saludo);
+        // model.addAttribute("mensaje", mensaje);
+        // // model.addAttribute("saludo", saludo);
+
+
+
         return "index";
     }
 

67ff7b79099e849cd5fb88c65f984c0fe80999e2
GaboSO21 <gso2090@gmail.com>
2022-12-21 21:00:47 -0800

    .

--- thexample/src/main/java/cr/webapp/thexample/controllers/PersonaController.java
+++ thexample/src/main/java/cr/webapp/thexample/controllers/PersonaController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import cr.webapp.thexample.repository.PersonaRepository;
 import cr.webapp.thexample.service.PersonaService;
 
 @Controller

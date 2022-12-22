3990c51e7ffe0223e58fa1efd8fb71d6dcdb8635
GaboSO21 <gso2090@gmail.com>
2022-12-21 23:36:03 -0800

    CRUD complete

--- mysqldemo/src/main/java/com/mysql/mysqldemo/controller/PersonaController.java
+++ mysqldemo/src/main/java/com/mysql/mysqldemo/controller/PersonaController.java
@@ -3,7 +3,9 @@ package com.mysql.mysqldemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.mysql.mysqldemo.domain.Persona;
 import com.mysql.mysqldemo.service.PersonaService;
 
 @Controller
@@ -16,11 +18,35 @@ public class PersonaController {
     }
 
     @GetMapping("/listar")
-    public String inicioPersonas(Model model) {
+    public String listar(Model model) {
 
         model.addAttribute("personas", personaService.listarPersonas());
 
-        return "index";
+        return "listar";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "agregar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        personaService.guardarPersona(persona);
+        return "redirect:/listar";
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona) {
+        personaService.eliminarPersona(persona);
+        return "redirect:/listar";
+    }
+
+    @GetMapping("/editar/{id_persona}")
+    public String editar(Persona persona, Model model) {
+        persona = personaService.buscarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "/agregar";
     }
 
 }
